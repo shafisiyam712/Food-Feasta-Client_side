03c30ff3701e21ce80292fc3f4c245bd00d79123
shafisiyam712 <shafisiyam674.com>
2025-05-12 14:35:28 +0600

    error page updated

--- src/Components/Blogs.jsx
+++ src/Components/Blogs.jsx
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 
 const Blogs = () => {
     return (
-        <div className="w-11/12 mx-auto mt-20">
+        <div className="w-11/12 mx-auto my-20">
             <h1 className="text-center font-bold text-3xl text-[#1E2A47] dark:text-white">Reed Health and Fitness blogs</h1>
             <p className="text-center mt-3">Check out experts thoughts and suggestions about health and fitness</p>
             <div className="w-11/12 mx-auto grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3 gap-2 mt-20">
-                <div className='card border border-rounded-xl border-[#1E2A47] shadow-xl overflow-hidden p-4 mb-2 gap-3'>
+                <div className='card border border-rounded-xl transition  hover:scale-105 shadow-xl overflow-hidden p-4 mb-2 gap-3'>
                     <div className='mb-2  space-y-4 '>
                         <img className='w-80 h-72 border rounded-full' src="https://i.ibb.co.com/k8Fpd72/Titcomb2019.webp" />
                     </div>
@@ -22,7 +22,7 @@ const Blogs = () => {
                     </div>
                 </div>
 
-                <div className='card border border-rounded-xl  border-[#1E2A47] shadow-xl overflow-hidden p-4 mb-2 gap-3'>
+                <div className='card border border-rounded-xl transition  hover:scale-105 shadow-xl overflow-hidden p-4 mb-2 gap-3'>
                     <div className='mb-2  space-y-4 '>
                         <img className='w-80 h-72 border rounded-full' src="https://i.ibb.co.com/8PCxnTm/beresini-bio.webp" />
                     </div>
@@ -35,7 +35,7 @@ const Blogs = () => {
                             to='https://www.outsideonline.com/health/nutrition/day-food-ketone-diet/' className="text-blue-500 hover:underline hover:text-blue-700" target="_blank">Reed Blog</Link>
                     </div>
                 </div>
-                <div className='card border border-rounded-xl  border-[#1E2A47] shadow-xl overflow-hidden p-4 mb-2 gap-3'>
+                <div className='card border border-rounded-xl transition  hover:scale-105 shadow-xl overflow-hidden p-4 mb-2 gap-3'>
                     <div className='mb-2  space-y-4 '>
                         <img className='w-80 h-72 border rounded-full' src="https://i.ibb.co.com/SNwdGZj/Dr.webp" />
                     </div>
